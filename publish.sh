e30b745ef83f690a9dc6e669547f717306bcfce3
Frank Gevaerts <frank@gevaerts.be>
2010-05-24 19:15:59 +0200

    Also publish plain source

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git push $HOME/inform/clouds/cloud-clone.git
 rsync -e ssh -a --delete -v $HOME/inform/clouds/cloud-clone.git/ gevaerts@shell.evonet.be:WWW/cloud.git/
-
+scp Source/story.ni gevaerts@shell.evonet.be:WWW/cloud.txt
