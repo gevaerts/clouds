314a63176c622c99e0a1b3ca14f222e1f457127a
Frank Gevaerts <frank@gevaerts.be>
2010-05-24 14:10:05 +0200

    fix rsync call

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git push $HOME/inform/clouds/cloud-clone.git
-rsync -e ssh -a -v $HOME/inform/clouds/cloud-clone.git/ gevaerts@shell.evonet.be:WWW/cloud.git/
+rsync -e ssh -a --delete -v $HOME/inform/clouds/cloud-clone.git/ gevaerts@shell.evonet.be:WWW/cloud.git/
 
