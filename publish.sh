c51881392400c5d35f72a5f252b274d0376096aa
Frank Gevaerts <frank@gevaerts.be>
2010-10-28 22:29:58 +0200

    remove obsolete publish script

--- publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git push $HOME/inform/clouds/cloud-clone.git
-rsync -e ssh -a --delete -v $HOME/inform/clouds/cloud-clone.git/ gevaerts@shell.evonet.be:WWW/cloud.git/
-rsync -e ssh -a --delete -v "$HOME/inform/clouds/cloud Materials/Release/" gevaerts@shell.evonet.be:WWW/cloud-web/
-#scp Source/story.ni gevaerts@shell.evonet.be:WWW/cloud.txt
